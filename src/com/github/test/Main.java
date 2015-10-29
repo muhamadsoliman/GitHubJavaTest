d6a0088270cb00ce73eb28cb25a20ffea9a6fc12
Mohamed Okasha <Mohamed.Okasha@EG01.Etisalat.net>
2015-10-29 11:12:29 +0200

    Change

---
+++ src/com/github/test/Main.java
@@ -0,0 +1,13 @@
+package com.github.test;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Hub!");
+	}
+
+}
